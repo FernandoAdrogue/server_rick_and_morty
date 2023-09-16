d3d56dfe086487ea3f9f4583b9174cb1b9b67c2b
FernandoAdrogue <chinoadrogue@gmail.com>
2023-09-16 12:56:00 -0300

    Fix conexion local index.js DB_connection.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { PORT , HOST } = process.env
 const {conn} = require("./src/DB_connection")
 const server = require("./src/app")
 
-conn.sync({force:true /* alter:true */}).then(()=>{
+conn.sync({/*force:true*/ alter:true }).then(()=>{
   server.listen(PORT,()=>{
     console.log(`Server raised in ${HOST}:${PORT}`);
   })
